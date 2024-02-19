c54ff98af0c599b0fa89d1268a2a26eccf2ce179
amirhosin <amirhosinshahsavan@gmail.com>
2024-02-20 01:10:21 +0330

    add get all user api add 3 image apis in user router.

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../../model/User.model");
+const path = require("path");
+const fs = require("fs");
+const auth = require("../../middleware/auth");
 
 router.post("/user/register", async function (req, res, next) {
-  const { firstname, lastname, email, password } = req.body;
+  const { firstname, lastname, email, password, type } = req.body;
 
   try {
     if (!email) {
@@ -20,6 +23,7 @@ router.post("/user/register", async function (req, res, next) {
         lastname: lastname,
         email: email,
         password: password,
+        type: type,
       }).save();
     }
     return res.status(200).json({
@@ -37,8 +41,20 @@ router.post("/user/register", async function (req, res, next) {
 router.post("/user/login", async function (req, res) {
   try {
     const { email, password } = req.body;
+    if (typeof email == "undefined" || email == "") {
+      return res.status(400).json({
+        code: -10001,
+        msg: "Enter Email",
+      });
+    }
+    if (typeof password == "undefined" || password == "") {
+      return res.status(400).json({
+        code: -10001,
+        msg: "Enter Password",
+      });
+    }
     var user = await User.findOne({ email });
-س
+    س;
     if (!user) {
       return res.status(404).json({
         code: -10001,
@@ -69,6 +85,105 @@ router.post("/user/login", async function (req, res) {
     return res.status(400).json({
       code: -10001,
       msg: "server crashed",
+    });
+  }
+});
+
+router.get("/user/getall", async (req, res, next) => {
+  try {
+    const userfound = User.find({
+      permission: "user",
+      type: "developer",
+    })
+      .select("_id firstname lastname image")
+      .limit(12)
+      .exec();
+    return res.status(200).json({
+      code: 102,
+      data: userfound,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      code: -10001,
+      msg: "server crashed",
+    });
+  }
+});
+
+////////////////////////////IMAGES API
+
+router.get("/user/image/:name", auth, async function (req, res, next) {
+  const userid = req.userAuth;
+  const userfound = await User.findOne({ _id: userid });
+  res.sendFile(
+    path.join(__dirname, "./../../file/images", userfound._id.toString())
+  );
+});
+
+router.post("/user/upload/", auth, async function (req, res) {
+  const userid = req.userAuth;
+  try {
+    const userfound = await User.findOne({ _id: userid });
+
+    if (!userfound) {
+      return res.status(404).json({
+        code: -10001,
+        msg: "user not found",
+      });
+    }
+    const buffer = Buffer.from(req.files.image.data);
+
+    const filePath = path.join(
+      __dirname,
+      "./../../file/images",
+      userfound._id.toString() + ".png"
+    );
+
+    var image_link = req.files.image.name;
+    await User.findOneAndUpdate(
+      { _id: userfound._id },
+      { $push: { image: image_link } }
+    );
+
+    fs.writeFileSync(filePath, buffer);
+    return res.status(200).json({
+      code: 10001,
+      msg: "File uploaded!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: -10001,
+      msg: error,
+    });
+  }
+});
+
+router.get("/user/upload/", auth, async function (req, res) {
+  const shenase_book = req.params.id;
+  try {
+    const bookfound = await Book.findOne({ shenase_book: shenase_book });
+    if (!bookfound) {
+      return res.status(404).json({
+        code: -10001,
+        msg: "book not found",
+      });
+    }
+
+    var lstsend = [];
+    bookfound.image.forEach((file) => {
+      lstsend.push("http://localhost:2000/user/image/:name" + file);
+    });
+    res.json({
+      code: 100,
+      msg: "get image link success",
+      data: {
+        file: lstsend,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: -10001,
+      msg: error,
     });
   }
 });
